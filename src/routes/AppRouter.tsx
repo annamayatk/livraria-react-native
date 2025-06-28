824c0aa67ae3acbba7a5e470d3b17b0aa1415801
annamayatk <annamayatk@gmail.com>
2025-06-27 21:43:21 -0300

    commit

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -2,8 +2,6 @@ import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
 import HomeProdutos from "../screens/HomeProdutos";
-import ListaDesejos from "../screens/ListaDesejos";
-import Avaliacoes from "../screens/Avaliacoes";
 import { Feather } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
