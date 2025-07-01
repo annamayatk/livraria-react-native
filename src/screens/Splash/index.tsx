b6d0a69650dcba7347da837de83da093ecc41d33
LGomes25 <leomybi@gmail.com>
2025-06-30 23:55:26 -0300

    Add: novos itens na Splash Screen

--- src/screens/Splash/index.tsx
+++ src/screens/Splash/index.tsx
@@ -19,12 +19,92 @@ export default function Splash({inicial}:SplashProps) {
 
     return (
         <View style={styles.container}>
+            
+            <View>
+                <View style={styles.containerCombo}>
+                {/* stars */}
+                <View style={styles.containerStars}>
+                    <LottieView
+                        source={require('../../img/alien.json')}
+                        autoPlay
+                        loop
+                        style={styles.animacaoStars}
+                        />
+                </View>
+
+                {/* ufo */}
+                <View style={styles.containerStars}>
+                    <LottieView
+                        source={require('../../img/stars.json')}
+                        autoPlay
+                        loop
+                        style={styles.animacaoUfo}
+                        />
+                </View>
+
+                {/* stars */}
+                <View style={styles.containerStars}>
+                    <LottieView
+                        source={require('../../img/moon.json')}
+                        autoPlay
+                        loop
+                        style={styles.animacaoStars}
+                        />
+                    </View>
+                </View>
+
+                <View style={styles.containerCombo}>
+                {/* stars */}
+                <View style={styles.containerStars}>
+                <LottieView
+                    source={require('../../img/stars.json')}
+                    autoPlay
+                    loop
+                    style={styles.animacaoStars}
+                    />
+                </View>
+
+                {/* ufo */}
+                <View style={styles.containerUfo}>
+                <LottieView
+                    source={require('../../img/ufo.json')}
+                    autoPlay
+                    loop
+                    style={styles.animacaoUfo}
+                    />
+                </View>
+
+                {/* stars */}
+                <View style={styles.containerStars}>
+                <LottieView
+                    source={require('../../img/stars.json')}
+                    autoPlay
+                    loop
+                    style={styles.animacaoStars}
+                    />
+                </View>
+                </View>
+            </View>
+            
+            {/* astronauta */}
+            <View style={styles.containerAstronauta}>
+                <LottieView
+                    source={require('../../img/anime2.json')}
+                    autoPlay
+                    loop
+                    style={styles.animacaoAstronauta}
+                    />
+            </View>
+
+            {/* livro */}
+            <View style={styles.containerLivro}>
             <LottieView
-                source={require('../../img/anime2.json')}
+                source={require('../../img/anime1.json')}
                 autoPlay
                 loop
-                style={styles.animacao}
+                style={styles.animacaoLivro}
                 />
+            </View>
         </View>
     )
 }
--- src/screens/Splash/style.ts
+++ src/screens/Splash/style.ts
@@ -4,21 +4,40 @@ import { StyleSheet } from 'react-native'
 export const styles = StyleSheet.create({
     container:{
         flex:1,
-        justifyContent:'center',
+        justifyContent:'space-between',
         alignItems:'center',
         backgroundColor:"black",
     },
-    animacao:{
+
+    containerCombo:{
+        flexDirection:'row',
+        alignItems:'stretch'
+    },
+
+    containerUfo:{},
+    animacaoUfo:{
+        width:150,
+        height:100,
+    },
+
+    containerStars:{},
+    animacaoStars:{
+        width:100,
+        height:100,
+    },
+
+    containerAstronauta:{
+        marginRight:100,
+    },
+    animacaoAstronauta:{
         width:200,
         height:200,
     },
-    logo:{
-        width:100, 
-        height:100,
-        marginBottom:20,
-    },
-    title:{
-        fontSize:24,
-        fontWeight:'bold',
+
+    containerLivro:{},
+    animacaoLivro:{
+        width:150,
+        height:150,
+        marginBottom:80,
     },
 });
